9e587c1a766aee70ab0649fa47a00622e4d612cd
adk-saugat <adhikarisaugat34@gmail.com>
2025-08-07 12:06:08 -0500

    bank app progress

---
+++ bank-app/app.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main(){
+	var balanceAmount float64 = 1000
+
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+	fmt.Print("Your Choice: ")
+
+	var choice int
+	fmt.Scan(&choice)
+
+	if choice == 1{
+		fmt.Printf("\nYour balance is $%v.", balanceAmount)
+	}else if choice == 2{
+		fmt.Printf("\nYour deposit amount: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+
+		balanceAmount += depositAmount
+
+		fmt.Printf("\nBalance updated! New Amount: $%v\n", balanceAmount)
+	}
+}
---
+++ bank-app/go.mod
@@ -0,0 +1,3 @@
+module github.com/bankApp
+
+go 1.24.5
