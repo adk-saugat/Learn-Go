3fde03b36dc2b924ca5b41504a7c212c0134ae6d
adk-saugat <adhikarisaugat34@gmail.com>
2025-08-10 11:23:12 -0500

    learning pointer

---
+++ pointers/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	age := 32 // regular variable
+	agePointer := &age // Pointer
+
+	fmt.Println("Age:", *agePointer)
+	editAgeToAdultYears(agePointer)
+	fmt.Printf("Age (adult years): %v",age)
+
+}
+
+func editAgeToAdultYears(age *int){
+	*age = *age - 18
+}
