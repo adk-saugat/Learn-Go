4358b86f94992fa6c9447ace011d285a9b041b3a
adk-saugat <adhikarisaugat34@gmail.com>
2025-08-20 09:38:03 -0500

    learning slices

--- lists/lists.go
+++ lists/lists.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	prices := [4]int{1,2,3,4}
+	prices := [5]int{1,2,3,4}
+	featuredPrices := prices[:3]
+	fmt.Println(featuredPrices)
+	
+	// Slice is just a reference from memory & can change the original value
+	featuredPrices[0] = 5
 	fmt.Println(prices)
+	fmt.Println(len(prices), cap(prices))
 }
